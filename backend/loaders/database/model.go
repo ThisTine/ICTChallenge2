c9810545a77f8ab1590c77edf7f7f510a7403732
BlueBox <pongsapukanuvech@gmail.com>
2023-11-19 22:59:54 +0700

    [FIX] remove question model

--- backend/loaders/database/model.go
+++ backend/loaders/database/model.go
@@ -9,7 +9,6 @@ var TopicModel *gorm.DB
 var TeamModel *gorm.DB
 var ScoreModel *gorm.DB
 var RawModel *gorm.DB
-var QuestionModel *gorm.DB
 var CardModel *gorm.DB
 
 func assignModel() {
@@ -17,7 +16,6 @@ func assignModel() {
 	TeamModel = DB.Model(new(database.Team))
 	ScoreModel = DB.Model(new(database.Score))
 	RawModel = DB.Model(new(database.Raw))
-	QuestionModel = DB.Model(new(database.Question))
 	CardModel = DB.Model(new(database.Card))
 
 }
