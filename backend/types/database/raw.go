7c98a1099e4fdc2d81338515d873b5f8b770fb67
Eve-nichakon <hello.eve555@gmail.com>
2023-11-19 22:30:12 +0700

    update: raw struct

--- backend/types/database/raw.go
+++ backend/types/database/raw.go
@@ -1,6 +1,8 @@
 package database
 
 type Raw struct {
-	TopicId []*Topic `json:"topics" gorm:"primaryKey;foreignKey:TopicId;references:Id"`
-	TeamId  []*Team  `json:"teams" gorm:"primaryKey;foreignKey:TeamId;references:Id"`
+	TopicId uint64 `json:"topics" gorm:"primaryKey;foreignKey:TopicId;references:Id"`
+	Topic   *Topic `json:"topic" gorm:"not null"`
+	TeamId  uint64 `json:"teams" gorm:"primaryKey;foreignKey:TeamId;references:Id"`
+	Team    *Team  `json:"team" gorm:"not null"`
 }
